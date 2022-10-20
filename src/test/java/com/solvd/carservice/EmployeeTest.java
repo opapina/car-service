b4c7f5eb7e4fac357dd3da9c63b9bc23e352c619
opapina <olgmixp@gmail.com>
2022-10-20 14:23:19 +0300

    test suite was created

--- src/test/java/com/solvd/carservice/EmployeeTest.java
+++ src/test/java/com/solvd/carservice/EmployeeTest.java
@@ -1,12 +1,18 @@
 package com.solvd.carservice;
 
+import com.solvd.carservice.domain.employee.Child;
 import com.solvd.carservice.domain.employee.Employee;
+import com.solvd.carservice.service.ChildService;
 import com.solvd.carservice.service.EmployeeService;
+import com.solvd.carservice.service.impl.ChildServiceImpl;
 import com.solvd.carservice.service.impl.EmployeeServiceImpl;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 public class EmployeeTest {
 
@@ -67,6 +73,45 @@ public class EmployeeTest {
         employee = employeeService.create(employee, 3L);
 
         Assert.assertNotNull(employee.getId(), "Employee id is null");
+    }
+
+    @Test(testName = "Verify that during creation employee children with Id will be created accordingly")
+    public void verifyEmployeeChildOnCreate() {
+        Child child1 = new Child();
+        child1.setFirstName("Anya");
+        child1.setLastName("Komleva");
+        child1.setDob(LocalDate.of(2016, 3, 27));
+
+        Child child2 = new Child();
+        child2.setFirstName("Dima");
+        child2.setLastName("Komlev");
+        child2.setDob(LocalDate.of(2011, 12, 13));
+
+        Employee employee = new Employee();
+        employee.setFirstName("Leonid");
+        employee.setLastName("Komlev");
+        employee.setDob(LocalDate.of(1999, 5, 12));
+        employee.setExperience(5);
+        employee.setProfession("designer");
+        employee.setChildren(Arrays.asList(child1, child2));
+
+        EmployeeService employeeService = new EmployeeServiceImpl();
+        employee = employeeService.create(employee, 15L);
+
+        ChildService childService = new ChildServiceImpl();
+        List<Child> children1 = childService.selectByName("Anya");
+        List<Child> children2 = childService.selectByName("Dima");
+        Long childId1Actual = employee.getChildren().get(0).getId();
+        Long childId2Actual = employee.getChildren().get(1).getId();
+        Long childId1Expected = children1.get(children1.size() - 1).getId();
+        Long childId2Expected = children2.get(children1.size() - 1).getId();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(childId1Actual, childId1Expected, "First child for employee is not created");
+        softAssert.assertEquals(childId2Actual, childId2Expected, "Second child for employee is not created");
+
+        softAssert.assertAll();
     }
 
     @AfterClass
